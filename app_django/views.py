3aee03edde05de60836490948f3212d697e58874
fenando-ff <fernandofreitassud2016@gmail.com>
2024-09-18 16:40:12 -0300

    dor_method_post

--- app_django/views.py
+++ app_django/views.py
@@ -11,6 +11,6 @@ def salvar(request):
     n_nome = request.POST.get("nome")
     n_email = request.POST.get("email")
     n_senha = request.POST.get("senha")
-    USUARIO.objects.create(nome= n_nome, email= n_email, senha = n_senha)
+    USUARIO.objects.create(nome = n_nome, email = n_email, senha = n_senha)
     usuario = USUARIO.objects.all()
-    return render(request, "index.html", {'usuarios': usuario})
+    return render(request, 'index.html', {'usuarios': usuario})
