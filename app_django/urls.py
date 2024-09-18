ff2175e43cf47d92b93b9bf443ee840b84d2cecb
fenando-ff <fernandofreitassud2016@gmail.com>
2024-09-18 15:59:05 -0300

    add_form

--- app_django/urls.py
+++ app_django/urls.py
@@ -4,5 +4,6 @@ from . import views
 #  Crie as rotas da views aqui
 
 urlpatterns = [
-    path('', views.listar_usuarios, name='listar_usuarios')
+    path('', views.listar_usuarios, name='listar_usuarios'),
+    path('salvar/', views.salvar, name='salvar')
 ]
