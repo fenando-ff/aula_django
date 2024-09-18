a01cb624a942bff958bf6686b54d17f56a0fe9b3
fenando-ff <fernandofreitassud2016@gmail.com>
2024-09-18 16:10:29 -0300

    add

--- app_django/views.py
+++ app_django/views.py
@@ -3,5 +3,14 @@ from .models import USUARIO
 # Create your views here.
 
 def listar_usuarios(request):
-    usuarios = USUARIO.objects.all()
-    return render(request, 'index.html', {'usuarios': usuarios})
+    usuario = USUARIO.objects.all()
+    return render(request, 'index.html', {'usuarios': usuario})
+
+
+def salvar(request):
+    n_nome = request.POST.get("nome")
+    n_email = request.POST.get("email")
+    n_senha = request.POST.get("senha")
+    USUARIO.objects.create(nome= n_nome, email= n_email, senha = n_senha)
+    usuario = USUARIO.objects.all()
+    return render(request, "index.html", {'usuarios': usuario})
