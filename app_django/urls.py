0546dd7b4a5d74dc1da6f286594e1a96610ebab5
fenando-ff <fernandofreitassud2016@gmail.com>
2024-09-18 17:49:04 -0300

    add_functions
    Adicionando as funções de adicionar, atualizar e apagar tuplas no banco de dados

--- app_django/urls.py
+++ app_django/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_usuarios, name='listar_usuarios'),
-    path('salvar/', views.salvar, name='salvar')
+    path('salvar/', views.salvar, name='salvar'),
+    path('editar/<int:id>', views.pegarId, name='editar'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.deletar, name='delete')
 ]
--- app_django/views.py
+++ app_django/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import USUARIO
 # Create your views here.
 
@@ -11,6 +11,26 @@ def salvar(request):
     n_nome = request.POST.get("nome")
     n_email = request.POST.get("email")
     n_senha = request.POST.get("senha")
-    USUARIO.objects.create(nome = n_nome, email = n_email, senha = n_senha)
+    USUARIO.objects.create(nome = n_nome, email = n_email, senha = n_senha) 
     usuario = USUARIO.objects.all()
     return render(request, 'index.html', {'usuarios': usuario})
+
+def pegarId(request,id):
+    usuarios = USUARIO.objects.get(id = id)
+    return render(request, 'update.html', {'usuario': usuarios})
+
+def update(request,id):
+    n_nome = request.POST.get("nome")
+    n_email = request.POST.get("email")
+    n_senha = request.POST.get("senha")
+    usuarios = USUARIO.objects.get(id = id)
+    usuarios.nome = n_nome
+    usuarios.email = n_email
+    usuarios.senha = n_senha
+    usuarios.save()
+    return redirect(listar_usuarios)
+
+def deletar(request,id):
+    usuarios = USUARIO.objects.get(id = id)
+    usuarios.delete()
+    return redirect(listar_usuarios)
